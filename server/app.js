6927ca02e99c28952d04827aec6b69dccfa77983
Hasan Sefa Ozalp <hasan@hasansefaozalp.com>
2020-03-03 15:47:30 -0800

    📚📗📙📘📕

---
+++ server/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const schema = require('./schema/schema')
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
+
+
+const PORT = process.env.PORT || 5000
+app.listen(3000, () => {
+  console.log(`Server started on port ${PORT}`)
+})
---
+++ server/schema/schema.js
@@ -0,0 +1,62 @@
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql')
+const _ = require('lodash')
+
+var books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' }
+]
+
+var authors = [
+  { name: 'Hasan Sefa Ozalp', age: 27, id: '1' },
+  { name: 'An other name', age: 44, id: '2' },
+  { name: 'Third name', age: 37, id: '3' }
+]
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, { id: parent.authorId })
+      }
+    }
+  })
+})
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(books, { id: args.id })
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id })
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+})
